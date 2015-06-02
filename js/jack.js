d17234855c44a906414500198032babba7f0a841
micvic <dryberg777@gmail.com>
2015-06-02 08:57:01 -0600

    Added files

---
+++ js/jack.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("nav a").click(function (evn) {
+        evn.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+    });
+
+});
